d85b9a82836b92b88c2522d0b6214c7247330f9b
Nurbusekaya <252c48djfm@privaterelay.appleid.com>
2026-06-02 00:32:02 +0300

    Update and rename Main.java to OrderSimulation.java

--- OrderSimulation.java
+++ OrderSimulation.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class OrderSimulation {
     public static void main(String[] args) {
         System.out.println("=== WELCOME TO OUR SMART ORDERING SYSTEM ===");
         // Müşteri Bilgileri)
